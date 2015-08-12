3a7536ccaa59166b9bbabc85608227e36ba1dbf4
joseterrera <joseterrera@yahoo.com>
2015-08-12 11:57:54 -0500

    favorited songs

---
+++ www/js/services.js
@@ -0,0 +1,14 @@
+angular.module('songhop.services', [])
+.factory('User', function(){
+	var o = {
+		favorites: []
+	}
+	o.addSongToFavorites = function(song){
+		//make sure there is a song to add
+		if(!song) return false;
+
+		//add to favorites array
+		o.favorites.unshift(song);
+	}
+	return o;
+});
