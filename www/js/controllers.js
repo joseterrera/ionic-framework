554400e4441e280129959d89bbf87c84516e2178
joseterrera <joseterrera@yahoo.com>
2015-08-19 16:20:39 -0500

    added loading indicators

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -4,11 +4,31 @@ angular.module('songhop.controllers', ['ionic', 'songhop.services'])
 /*
 Controller for the discover page
 */
-.controller('DiscoverCtrl', function($scope, $timeout, User, Recommendations) {
+.controller('DiscoverCtrl', function($scope, $ionicLoading, $timeout, User, Recommendations) {
+  // helper functions for loading
+  var showLoading = function() {
+    $ionicLoading.show({
+      template: '<i class="ion-loading-c"></i>',
+      noBackdrop: true
+    });
+  }
+
+  var hideLoading = function() {
+    $ionicLoading.hide();
+  }
+
+  // set loading to true first time while we retrieve songs from server.
+  showLoading();
+
    Recommendations.init()
     .then(function(){
       $scope.currentSong = Recommendations.queue[0];
-      Recommendations.playCurrentSong();
+      return Recommendations.playCurrentSong();
+  })
+    .then(function(){
+        //turn loading off
+        hideLoading();
+        $scope.currentSong.loaded = true;
     });
 
   //fired when we favorite /skip a song
@@ -25,6 +45,9 @@ Controller for the discover page
         // $timeout to allow animation to complete
       $scope.currentSong = Recommendations.queue[0];
     }, 250);
+    Recommendations.playCurrentSong().then(function() {
+      $scope.currentSong.loaded = true;
+    });
     }
     //used for retrieving the next album image.
     //if there isn't an album image available next, return empty string.
